bd54888210f0704e340344d67ab3507677e2c2e0
Maryam Lotfi <lotfimaryam1110@gmail.com>
2025-06-14 04:56:33 -0900

    database change postgres

--- doctor_online/settings.py
+++ doctor_online/settings.py
@@ -73,13 +73,24 @@ WSGI_APPLICATION = 'doctor_online.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'doctor_online_db',  # نام دیتابیس که ایجاد کرده‌اید
+        'USER': 'yekta',            # نام کاربری که ایجاد کرده‌اید
+        'PASSWORD': '13771110',     # رمز عبور 사용자
+        'HOST': 'localhost',          # یا '127.0.0.1'
+        'PORT': '5432',               # پورت پیش‌فرض برای PostgreSQL
 
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
